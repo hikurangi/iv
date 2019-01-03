069ff14b158fee24cf87f52276257f3617245c8d
Hikurangi <hikurangi@gmail.com>
2019-01-04 09:15:33 +1300

    refactor, simplify drastically

--- src/wins/column.js
+++ src/wins/column.js
@@ -1,49 +1,33 @@
-import { any, filter, groupBy, path, values }           from 'rambda'
-import { getIsConsecutive, getLengthMeetsWinCondition } from '../helpers'
+import { any, flatten, groupBy, pipe, values } from 'rambda'
 
-const hasWinningColumn = ({ currentPlayerCells, winCondition }) => {
+const hasWinningColumn = ({ board, currentPlayer, winCondition }) => {
   
-  const byColumn = groupBy(cell => cell.column, currentPlayerCells)
-  const winCandidateColumns = values(filter(column => column.length >= winCondition, byColumn))
-
-  function checkColumns () {
+  const getColumns = pipe(
+    flatten,
+    groupBy(cell => cell.column),
+    values
+  )
+  
+  const boardByColumns = getColumns(board)
+  
+  return any(column => {
+    let consecutives = 0
     
-    return any(column => {
-      let consecutives = []
-      
-      for (let i = 0; i < column.length; i++) {
-        const thisRow = column[i].row
-        const prevRow = path(`${i - 1}.row`, column)
-        
-        // a single number is always consecutive, the first one will always be a single number
-        if (i === 0) {
-          consecutives = [thisRow]
-          continue
-        }
-        
-        if (getIsConsecutive(prevRow, thisRow)) {
-          consecutives.push(thisRow)
+    for (let i = 0; i < column.length; i++) {
+      const currentCell = column[i]
 
-          if (getLengthMeetsWinCondition({ consecutives, winCondition })) {
-            return true
-          }
-          continue
-        }
-        
-        if (!getIsConsecutive(...consecutives) || i === column.length) {
-          consecutives = []
-          continue
-        }
-        
-        if (getLengthMeetsWinCondition({ consecutives, winCondition })) {
-          return true
-        }
-
+      if (currentCell.player === currentPlayer) {
+        consecutives++
+      } else {
+        consecutives = 0
       }
-    }, winCandidateColumns)
-  }
-  
-  return checkColumns()
+
+      if (consecutives >= winCondition) {
+        return true
+      }
+    }
+
+  }, boardByColumns)
 }
 
 export default hasWinningColumn
--- src/wins/row.js
+++ src/wins/row.js
@@ -1,49 +1,24 @@
-import { any, filter, groupBy, path, values }           from 'rambda'
-import { getIsConsecutive, getLengthMeetsWinCondition } from '../helpers'
+import { any } from 'rambda'
 
-const hasWinningRow = ({ currentPlayerCells, winCondition }) => {
-  
-  const byRow = groupBy(cell => cell.row, currentPlayerCells)
-  const winCandidateRows = values(filter(row => row.length >= winCondition, byRow))
-
-  function checkRows () {
+const hasWinningRow = ({ board, currentPlayer, winCondition }) => {
+  return any(row => {
+    let consecutives = 0
     
-    return any(row => {
-      let consecutives = []
-      
-      for (let i = 0; i < row.length; i++) {
-        const thisColumn = row[i].column
-        const prevColumn = path(`${i - 1}.column`, row)
-        
-        // a single number is always consecutive, the first one will always be a single number
-        if (i === 0) {
-          consecutives = [thisColumn]
-          continue
-        }
-        
-        if (getIsConsecutive(prevColumn, thisColumn)) {
-          consecutives.push(thisColumn)
+    for (let i = 0; i < row.length; i++) {
+      const currentCell = row[i]
 
-          if (getLengthMeetsWinCondition({ consecutives, winCondition })) {
-            return true
-          }
-          continue
-        }
-        
-        if (!getIsConsecutive(...consecutives) || i === row.length) {
-          consecutives = []
-          continue
-        }
-        
-        if (getLengthMeetsWinCondition({ consecutives, winCondition })) {
-          return true
-        }
-
+      if (currentCell.player === currentPlayer) {
+        consecutives++
+      } else {
+        consecutives = 0
       }
-    }, winCandidateRows)
-  }
-  
-  return checkRows()
+
+      if (consecutives >= winCondition) {
+        return true
+      }
+    }
+
+  }, board)
 }
 
 export default hasWinningRow
